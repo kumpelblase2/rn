d7d67738a653d6995055c560f9ff7c95d9366ad4
donnerruebe <jan.rub@gmx.de>
2015-05-31 23:14:42 +0200

    Transmitting of any File works. Timeout and RTO works. Parallel Transmit works

--- Aufgabe3/client/client.py
+++ Aufgabe3/client/client.py
@@ -1,10 +1,15 @@
 import time
 import socket
 import select
+import struct
 
 UDP_IP = "localhost"
 UDP_PORT = 8080
 MESSAGE = "Hello, World!"
+
+transferlimit=6
+
+
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
@@ -13,20 +18,175 @@ print "message:", MESSAGE
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
+def readFile(fn,chunksize):
+    f = open(fn, "rb")
+    try:
+        chunk=[];
+        blocks=[];
+        byte = f.read(1)
+        while byte != "":
+            # Do stuff with byte.
+            chunk.append(byte)
+            if(len(chunk)>=chunksize):
+                print "Chunk(%d)"%(len(blocks),),chunk
+                blocks.append({"chunk":chunk,"send":0,"ack":0,"sendtime":0,"timeout":0,"firstsend":0})
+                chunk=[]
+            byte = f.read(1)
+        blocks.append({"chunk":chunk,"send":0,"ack":0,"sendtime":0,"timeout":0,"firstsend":0})
+    finally:
+        f.close()
+    return blocks
+                     
+                     
+def sequenznum(id=0):
+    return struct.pack("Q", id)
+                     
+def firstmessage(PATH,SIZE,ERRORRATE):
+    return "%s%s;%d;%d"%(sequenznum(),PATH,SIZE,ERRORRATE)
+
+def datamessage(id,chunk):
+    data=''.join(chunk)
+    return "%s%s"%(sequenznum(id),data)
+
+                     
 def readUDP():
-    readable_list = [sock]
-    readable, writeable, errord = select.select(readable_list, [], [])
-    for s in readable:
+    r, w, e = select.select([sock], [], [],0)
+    for s in r:
         if s is sock:
             data, addr = sock.recvfrom(1024)
-            print data,addr 
-                     
-for i in range(20):
-    sock.sendto("%s, %d"%(MESSAGE,i), (UDP_IP, UDP_PORT))  
-    sock.recv
-    print "message:", "%s, %d"%(MESSAGE,i)
-    time.sleep(.1)
+            
+            if len(data)==8:
+                paketID=struct.unpack("Q",data[0:8])[0]
+                
+                print "Pack %d was ACK"%(paketID,)
+                if(paketID>0):
+                    setPacketAck(paketID)
+                    paketRTT=time.time()-blocks[paketID-1]["sendtime"]
+                    print "RTT:",(paketRTT)
+            else:
+                print "WRONG ANSWER:",data,addr
+            
+
+def testComplete():
+    for block in blocks:
+        if block.get("ack",0)==0:
+            return False
+    return True
+
+def setPacketAck(id):
+    newRTT=time.time()-blocks[id-1]["sendtime"]
+    RTT.calcRTO(newRTT)
+    blocks[id-1]["ack"]=1
+    
+    
+def packetsPending():
+    count=0
+    for block in blocks:
+        if(block.get("ack",0)==0 and block.get("send",0)!=0):
+            count+=1
+    return count
+
+def getNextBlockID():
+    for i,block in enumerate(blocks):
+        if(block.get("send",0)==0):
+            #print "nextID found ",i
+            return i
+    return None
+            
+
+
+ 
+WindowSize=50
+ErrorRate=100
+
+def sendPacket(id):
+    blocks[id]["send"]+=1
+    blocks[id]["timeout"]=time.time()+RTT.RTO
+    blocks[id]["sendtime"]=time.time()
+    if(blocks[id].get("firstsend",0)==0):
+        blocks[id]["firstsend"]=time.time()
+    MESSAGE = datamessage(id+1,blocks[id]["chunk"])
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    
+def checkTimeout():
+    for i,block in enumerate(blocks):
+        if(block.get("ack",0)==0 and block.get("send",0)!=0):
+            if(block.get("timeout",0) < time.time()):
+                print "retransmit",i
+                sendPacket(i)
+                RTT.doubleRTO()
+                
+
+class RetransmitTime:
+
+    def __init__(self):
+        self.RTOmax=2
+        self.RTO=1
+        self.SRTT=1
+        self.rttVar=0.5
+        self.beta=0.25
+        self.alpha=0.125
+        self.first=True
+    
+    def calcRTO(self,newRTT):
+        if(self.first==True):
+            self.SRTT=newRTT
+            self.rttVar=0.5*newRTT 
+            self.first=False
+        self.rttVar  = (1-self.beta) * self.rttVar + self.beta * abs(self.SRTT - newRTT)
+        self.SRTT    = (1-self.alpha) * self.SRTT + self.alpha * newRTT
+        self.RTO     = self.SRTT + 4 * self.rttVar 
+        if self.RTO > self.RTOmax:
+            print "RTOMAX!!!",self.RTO,
+            self.RTO=self.RTOmax
+        print "RTO",(newRTT,self.RTO,self.SRTT,self.rttVar)
+      
+    def doubleRTO(self):
+        self.RTO+=self.RTO
+        if self.RTO > self.RTOmax:
+            self.RTO=self.RTOmax    
+
+
+RTT=RetransmitTime()
+            
+blocks=readFile("file.pdf",WindowSize)
+
+'''
+for i,block in enumerate(blocks):
+    MESSAGE=datamessage(i+1,block["chunk"])
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))  
+    print "message:", "%s"%(MESSAGE,)
+    time.sleep(.001)
     readUDP()
-    
-    
+'''
+
+
+sock.sendto(firstmessage("a/speciel/dir/file.pdf",WindowSize,ErrorRate) ,(UDP_IP, UDP_PORT))
+
+while not testComplete():
+    time.sleep(.001) 
+    readUDP()
+    if packetsPending()<transferlimit:
+        #print "send more Packets"
+        nextID=getNextBlockID()
+        #print "next",nextID
+        if (not nextID == None):
+            #print "next",nextID
+            sendPacket(nextID)
+    checkTimeout()
+
+sock.sendto("FIN", (UDP_IP, UDP_PORT))      
+
+       
+print testComplete()
+
+blocks[0]["send"]=1
+print getNextBlockID()
+print packetsPending()
+
+for block in blocks:
+    block["ack"]=1
+
+print testComplete()
+
 time.sleep(3)
--- Aufgabe3/server/server.py
+++ Aufgabe3/server/server.py
@@ -6,11 +6,17 @@
 
 import time
 import socket
+import select
 import sys
+import struct
+import os
 
-UDP_IP = "localhost"
+UDP_IP = "0.0.0.0"
 UDP_PORT = 8080
 MESSAGE = "Hello, World!"
+
+path=""
+windowSize=0
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
@@ -20,15 +26,86 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind( (UDP_IP,UDP_PORT))
 #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
+errorrate=10
+errorcount=0
+ackresponse=[]
+pakets=dict()
 
+def sequenznum(id=0):
+    return struct.pack("Q", id)
 
+def addResponse(msg,timetosend):
+    global errorcount,errorrate
+    errorcount+=1
+    if errorcount%errorrate!=0:
+        ackresponse.append({"msg":msg,"time":timetosend,"send":0})
+    else:
+        print "ACK DROPPED",msg
 
+def getAckResponse():
+    for item in ackresponse:
+        if item["time"]<time.time():
+            ackresponse.remove(item)
+            return item
+    
+class conLock:
+    def __init__(self,TO=5):
+        self.lock = False
+        self.addr = "0.0.0.0"
+        self.TOtime = TO
+        self.timeout = 0
+    
+    def setlock(self, addr = None):
+        if(not self.lock):
+            print "+ Locked"
+            self.addr = addr
+            self.lock = True
+            self.clearTimeout()
+            pakets=dict()
+    
+    def unlock(self,reason = ""):
+        if(self.lock):
+            print "- Unlocked: %s"%(reason,)
+            ackresponse=[]
+            self.lock=False
+        
+    def clearTimeout(self):
+        rest = (self.timeout-time.time())
+        self.timeout = time.time()+self.TOtime
+        return rest
+    
+    def checkTime(self):
+        if(time.time()>self.timeout):
+            self.unlock("Timeout")
+        
+    def isLocked(self):
+        return self.lock
+    
+    def conAccept(self,addr = None):
+        if(self.lock and addr == self.addr):
+            print "Timeout cleared, rest was:%f"%self.clearTimeout()
+        return addr == self.addr or not self.lock
 
-
-
-
-
+def writeToFile(pakets):
+    print "writing File to: %s"%(path,)
+    dir=os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    f = open(path, "wb")
+    try:
+        for line in pakets:
+            f.write(pakets[line])
+    finally:
+        f.close()
+        
+def initTransfer(param):
+    global path
+    path,windowSize,errorrate=param
+    print "Starte Dateitransfer nach:\n%s"%(path,)
+    return True #TODO CHECK
+        
 def run():
+    Locker = conLock(5)
     print( "Starte Filetransferserver\n" )
     print( "===========================" )
     print( "UDP: [%s:%d]"%(UDP_IP,UDP_PORT) )
@@ -36,17 +113,52 @@ def run():
     
     print( "= Now Listening ===========" )
     print( "===========================" )
-    while True:    
-        try:
-            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-            print "Got data from", addr
-            print "received message:", data
-            print( "===========================" )
-            sock.sendto("received: '%s'"%(data,), addr)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            traceback.print_exc()
+    while True:
+        Locker.checkTime()
+        time.sleep(0.001)
+        
+        
+        
+        r,w,x = select.select([sock],[],[],0)
+        for s in r:
+            if s is sock:
+                data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+                if(Locker.conAccept(addr = addr)):
+                    if len(data)<8:
+                        if data=="FIN":
+                            for line in pakets:
+                                print line,pakets[line]
+                            writeToFile(pakets)
+                            Locker.unlock("Final Packet")
+                    else:
+                        if(struct.unpack("Q",data[0:8])[0]==0):
+                            print "=== FIRST-MESSAGE ==="
+                            param=data[8:].split(";")
+                            print param
+                            destinationPath=param[0]
+                            windowSize=param[1]
+                            ErrorRate=param[2]
+                            if(initTransfer(param)):
+                                Locker.setlock(addr)
+                        
+                        
+                        if(Locker.isLocked()):
+                            packetnr=struct.unpack("Q",data[0:8])[0]
+                            transfer=data[8:]
+                            print "Got data from", addr
+                            print "PacketNR.:",packetnr
+                            print "received message:", transfer, len(transfer)
+                            print( "===========================" )
+                            if(packetnr>0):
+                                pakets[packetnr]=transfer
+                            addResponse(sequenznum(packetnr),time.time()+0.01)
+                            #sock.sendto("received: '%s'"%(data,), addr)
+            
+        if(Locker.isLocked()):
+            packet=getAckResponse()
+            if packet:
+                sock.sendto(packet["msg"], addr)
+
     
 
 if __name__ == "__main__":
